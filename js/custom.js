4f6a0839f65ad9e83cffd42710f4b6535d0035de
webdeveloper <webdeveloper@Bihans-MacBook-Pro.local>
2021-07-11 21:05:14 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+//this is a comment
+document.write("Hello World from JavaScript");
+
+document.getElementById("myTxt").innerHTML = "New Text";
+document.getElementById("myTxt").style.display = "block";
